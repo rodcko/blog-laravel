32c106d6db79802c2ef5de968a7546740d3e20a3
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-03-28 00:20:48 -0600

    Actualizando nuestros posts

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Post;
 use App\Http\Requests\PostRequest;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -71,9 +72,17 @@ class PostController extends Controller
      * @param  \App\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Post $post)
+    public function update(PostRequest $request, Post $post)
     {
-        //
+        //dd($request->all());
+        $post->update($request->all());
+            if ($request->file('file')){
+            Storage::disk('public')->delete($post->image);
+            $post->image = $request->file('file')->store('posts', 'public');
+            $post->save();
+        }
+
+        return back()->with('status', 'Actualizado con exito');
     }
 
     /**
@@ -84,6 +93,9 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Storage::disk('public')->delete($post->image);
+        $post->delete();
+
+        return back()->with('status', 'Eliminado con exito');
     }
 }
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -29,7 +29,7 @@
                       </div>                      
                       <div class="form-group">
                         <label>Contenido embebido</label>
-                        <textarea name="iframe" class="form-control" required>{{ old('iframe', $post->iframe) }}</textarea>
+                        <textarea name="iframe" class="form-control">{{ old('iframe', $post->iframe) }}</textarea>
                       </div>
                       <div class="form-group">
                         @csrf
